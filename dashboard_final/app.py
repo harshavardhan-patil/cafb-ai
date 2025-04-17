7db5e04343c141e63286bbb82aac7130491d5879
KritiShahi <kritishahi66@gmail.com>
2025-04-17 15:42:28 -0400

    Dashboard Files

---
+++ dashboard_final/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from src.config import RAW_DATA_DIR
+import base64
+from loguru import logger
+import plotly.graph_objects as go
+
+st.set_page_config(layout="wide")
+
+pg = st.navigation([st.Page("src/pages/Overview.py"), st.Page("src/pages/Trends.py"),  st.Page("src/pages/Topic Modelling.py"), st.Page("src/pages/Complaints Information.py")])
+pg.run()
